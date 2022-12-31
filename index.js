1fc7084e4b49a4c2fd2d56149bd5bc140d287cc0
salmanaugmentler <salman@augmentler.com>
2022-12-31 22:05:36 +0500

    remove helmet

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ dotenv.config();    // Load environment variables from .env file
 const app = express();  // Initialize express app
 app.use(express.json());    // Parse JSON bodies
 app.use(helmet());  // Set security HTTP headers
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" })); // Set cross-origin resource policy
+// app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" })); // Set cross-origin resource policy
 // app.use(helmet.referrerPolicy({ policy: "cross-origin" })); // Set referrer policy
 app.use(morgan("common"));  // Log HTTP requests
 app.use(bodyParser.json({ limit: "30mb", extended: true }));  // Parse JSON bodies
